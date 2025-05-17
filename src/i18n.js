479663a2df0c685b23b38ecb18aaa8818cef39c2
Steatt <simon.boisneault@gmail.com>
2025-05-17 17:10:44 +0300

    final commit

--- src/i18n.js
+++ src/i18n.js
@@ -2,8 +2,8 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-import translationEN from "@/locales/en/translation.json";
-import translationFR from "@/locales/fr/translation.json";
+import translationEN from "@/locales/EN/translation.json";
+import translationFR from "@/locales/FR/translation.json";
 
 i18n
   .use(LanguageDetector)
